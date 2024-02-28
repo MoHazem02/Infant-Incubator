b8ecf84edb0f5c4ce0b412f371b889f918b594b2
Mohamed Hazem <mohamedhazemwwe@gmail.com>
2024-02-28 21:35:07 +0200

    Added Monitoring LCD and Temp Img

--- Incubator.py
+++ Incubator.py
@@ -1,16 +1,18 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from Assets import Resources_rc
 import sys
+from Assets import Resources_rc
+
+
 
 class Ui_Monitor(object):
     def setupUi(self, Monitor):
         Monitor.setObjectName("Monitor")
-        Monitor.resize(891, 631)
+        Monitor.resize(900, 650)
+        Monitor.setMinimumSize(QtCore.QSize(880, 600))
+        Monitor.setMaximumSize(QtCore.QSize(900, 650))
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap("Assets/Incubator.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         Monitor.setWindowIcon(icon)
-        Monitor.setMaximumSize(QtCore.QSize(900, 650))
-        Monitor.setMinimumSize(QtCore.QSize(880, 550))
         Monitor.setStyleSheet("background-color:#3A3B3C;\n" "color: white;")
         self.centralwidget = QtWidgets.QWidget(Monitor)
         self.centralwidget.setObjectName("centralwidget")
@@ -40,50 +42,6 @@ class Ui_Monitor(object):
         self.BPM_Frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.BPM_Frame.setObjectName("BPM_Frame")
         self.gridLayout.addWidget(self.groupBox, 0, 1, 1, 1)
-        self.groupBox_3 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_3.setObjectName("groupBox_3")
-        self.set_button = QtWidgets.QPushButton(self.groupBox_3)
-        self.set_button.setGeometry(QtCore.QRect(230, 180, 181, 31))
-        self.set_button.setMaximumSize(QtCore.QSize(200, 16777215))
-        self.set_button.setStyleSheet("background-color: #784B84;\n"
-"    color: white;\n"
-"    border: none;\n"
-"    padding: 5px 10px;\n"
-"     border: 1.2px ;\n"
-"border-style: outset;\n"
-"border-radius: 8px;")
-        self.set_button.setObjectName("set_button")
-        self.temp_LCD = QtWidgets.QLCDNumber(self.groupBox_3)
-        self.temp_LCD.setGeometry(QtCore.QRect(20, 70, 151, 91))
-        self.temp_LCD.setObjectName("temp_LCD")
-        self.increase_button = QtWidgets.QPushButton(self.groupBox_3)
-        self.increase_button.setEnabled(True)
-        self.increase_button.setGeometry(QtCore.QRect(330, 110, 91, 31))
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        self.increase_button.setFont(font)
-        self.increase_button.setStyleSheet("background-color: #00A86B;\n"
-"    color: white;\n"
-"    border: none;\n"
-"    padding: 5px 10px;\n"
-"     border: 1.2px ;\n"
-"border-style: outset;\n"
-"border-radius: 8px;")
-        self.increase_button.setObjectName("increase_button")
-        self.decrease_button = QtWidgets.QPushButton(self.groupBox_3)
-        self.decrease_button.setGeometry(QtCore.QRect(230, 110, 91, 31))
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        self.decrease_button.setFont(font)
-        self.decrease_button.setStyleSheet("background-color: #E0115F;\n"
-"    color: white;\n"
-"    border: none;\n"
-"    padding: 5px 10px;\n"
-"     border: 1.2px ;\n"
-"border-style: outset;\n"
-"border-radius: 8px;")
-        self.decrease_button.setObjectName("decrease_button")
-        self.gridLayout.addWidget(self.groupBox_3, 1, 0, 1, 1)
         self.frame = QtWidgets.QFrame(self.centralwidget)
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
@@ -99,9 +57,65 @@ class Ui_Monitor(object):
         self.Status_Frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.Status_Frame.setObjectName("Status_Frame")
         self.gridLayout.addWidget(self.frame, 1, 1, 1, 1)
+        self.groupBox_3 = QtWidgets.QGroupBox(self.centralwidget)
+        self.groupBox_3.setObjectName("groupBox_3")
+        self.set_button = QtWidgets.QPushButton(self.groupBox_3)
+        self.set_button.setGeometry(QtCore.QRect(160, 210, 181, 31))
+        self.set_button.setMaximumSize(QtCore.QSize(200, 16777215))
+        self.set_button.setStyleSheet("background-color: #784B84;\n"
+"    color: white;\n"
+"    border: none;\n"
+"    padding: 5px 10px;\n"
+"     border: 1.2px ;\n"
+"border-style: outset;\n"
+"border-radius: 8px;")
+        self.set_button.setObjectName("set_button")
+        self.temp_LCD = QtWidgets.QLCDNumber(self.groupBox_3)
+        self.temp_LCD.setGeometry(QtCore.QRect(10, 160, 111, 61))
+        self.temp_LCD.setObjectName("temp_LCD")
+        self.increase_button = QtWidgets.QPushButton(self.groupBox_3)
+        self.increase_button.setEnabled(True)
+        self.increase_button.setGeometry(QtCore.QRect(270, 170, 91, 31))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.increase_button.setFont(font)
+        self.increase_button.setStyleSheet("background-color: #00A86B;\n"
+"    color: white;\n"
+"    border: none;\n"
+"    padding: 5px 10px;\n"
+"     border: 1.2px ;\n"
+"border-style: outset;\n"
+"border-radius: 8px;")
+        self.increase_button.setObjectName("increase_button")
+        self.decrease_button = QtWidgets.QPushButton(self.groupBox_3)
+        self.decrease_button.setGeometry(QtCore.QRect(160, 170, 91, 31))
+        font = QtGui.QFont()
+        font.setPointSize(9)
+        self.decrease_button.setFont(font)
+        self.decrease_button.setStyleSheet("background-color: #E0115F;\n"
+"    color: white;\n"
+"    border: none;\n"
+"    padding: 5px 10px;\n"
+"     border: 1.2px ;\n"
+"border-style: outset;\n"
+"border-radius: 8px;")
+        self.decrease_button.setObjectName("decrease_button")
+        self.temp_LCD_2 = QtWidgets.QLCDNumber(self.groupBox_3)
+        self.temp_LCD_2.setGeometry(QtCore.QRect(30, 40, 161, 71))
+        self.temp_LCD_2.setObjectName("temp_LCD_2")
+        self.label_2 = QtWidgets.QLabel(self.groupBox_3)
+        self.label_2.setGeometry(QtCore.QRect(20, 230, 81, 21))
+        self.label_2.setObjectName("label_2")
+        self.BPM_Frame_2 = QtWidgets.QFrame(self.groupBox_3)
+        self.BPM_Frame_2.setGeometry(QtCore.QRect(240, 40, 120, 80))
+        self.BPM_Frame_2.setStyleSheet("image: url(:/newPrefix/thermometer.png);")
+        self.BPM_Frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        self.BPM_Frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.BPM_Frame_2.setObjectName("BPM_Frame_2")
+        self.gridLayout.addWidget(self.groupBox_3, 1, 0, 1, 1)
         Monitor.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(Monitor)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 891, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 900, 26))
         self.menubar.setObjectName("menubar")
         Monitor.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(Monitor)
@@ -116,11 +130,12 @@ class Ui_Monitor(object):
         Monitor.setWindowTitle(_translate("Monitor", "Incubator Monitor"))
         self.groupBox_2.setTitle(_translate("Monitor", "Humidity"))
         self.groupBox.setTitle(_translate("Monitor", "Heart Rate"))
+        self.label.setText(_translate("Monitor", "Status"))
         self.groupBox_3.setTitle(_translate("Monitor", "Temperature"))
         self.set_button.setText(_translate("Monitor", "SET"))
         self.increase_button.setText(_translate("Monitor", "+"))
         self.decrease_button.setText(_translate("Monitor", "-"))
-        self.label.setText(_translate("Monitor", "Status"))
+        self.label_2.setText(_translate("Monitor", "Control"))
 
 
 if __name__ == "__main__":
