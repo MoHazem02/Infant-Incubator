0b0cc1110ffbcb2cb0e6c9dc480ce1bdaa54592c
Omar-Khaled-19 <omarkhaled191104@gmail.com>
2024-02-28 23:36:30 +0200

    second stage done

--- ApplicationManager.py
+++ ApplicationManager.py
@@ -5,6 +5,7 @@ class ApplicationManager:
     def __init__(self,UI):
         self.ui = UI
         self.serial_port = Serial('COM4', 9600)
+        self.boundry_temp = 45
         self.start()
         
     def start(self):
@@ -15,13 +16,19 @@ class ApplicationManager:
     def read_data(self):
         arduino_data = self.serial_port.readline().decode('utf-8').strip()
         if arduino_data:
-            temprature_value, str2 = arduino_data.split(',')
-            humidity, heart_rate = str2.split(';')
+            temprature, str2 = arduino_data.split(';')
+            humidity, heart_rate = str2.split(',')
             
             self.ui.humidity_LCD.display(humidity)
             self.ui.BPM_LCD.display(heart_rate)
-            self.ui.temp_LCD.display(heart_rate)
+            self.ui.temp_LCD_2.display(temprature)
+            self.ui.temp_LCD.display(self.boundry_temp)
 
-                
-        
+            if (int(temprature) <= self.boundry_temp):
+                self.ui.Status_Frame.setStyleSheet("image: url(:/newPrefix/warning.png);")
+    
+    def increment_boundry_temp(self):
+        self.boundry_temp += 1
 
+    def decrement_boundry_temp(self):
+        self.boundry_temp -= 1
